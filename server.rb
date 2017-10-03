adeb1a2ded4cbb2e7839d572c3ebbe8e7fe391f5
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-10-03 15:13:18 -0400

    Add about variables

--- server.rb
+++ server.rb
@@ -7,3 +7,10 @@ end
 get '/portfolio' do
   erb :gallery
 end
+
+
+get '/about_me' do
+  erb :about
+  @skills    = ['git', 'HTML', 'CSS', 'Ruby']
+  @interests = ['Cats', 'Art', 'Music', "Films", "Tea"] 
+end
