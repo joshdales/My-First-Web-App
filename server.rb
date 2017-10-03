d1401bc2d9b6844faff1500fcd4d74d21b3958d3
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-10-03 15:19:15 -0400

    Add list of skills to about me page

--- server.rb
+++ server.rb
@@ -10,7 +10,7 @@ end
 
 
 get '/about_me' do
-  erb :about
   @skills    = ['git', 'HTML', 'CSS', 'Ruby']
-  @interests = ['Cats', 'Art', 'Music', "Films", "Tea"] 
+  @interests = ['Cats', 'Art', 'Music', "Films", "Tea"]
+  erb :about
 end
