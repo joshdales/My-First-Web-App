52de07127369169d965eb1231590ea7d78eee35a
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-10-03 15:26:19 -0400

    Create fav_links variables for favourites

--- server.rb
+++ server.rb
@@ -14,3 +14,7 @@ get '/about_me' do
   @interests = ['Cats', 'Art', 'Music', "Films", "Tea"]
   erb :about
 end
+
+get '/favourites' do
+  @fav_links = ['https://www.feedly.com', 'https://play.google.com', 'https://www.github.com', 'https://www.bitmaker.co', 'https://www.royalcollection.org.uk/collection/themes/trails/frames-in-the-royal-collection#/'']
+end
